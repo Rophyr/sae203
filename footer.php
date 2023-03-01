39f425c91e4b302ccc86b57a0395573a4d0f436e
Rophyr <rophyr11@gmail.com>
2023-03-01 14:46:10 +0100

    Test commit
    :100:

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
   <div class="footer_content">
     <div class="footer_links">
       <ul>
-        <li><p>Copyright</p></li>
+        <li><p>Copyright © Enzo Latu</p></li>
       </ul>
     </div>
   </div>
